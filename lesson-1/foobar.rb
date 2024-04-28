ba50abdcd3dde6a608c4240fca4ecdcbfa100036
Alexander Posukhovskiy <extrime74@gmail.com>
2024-04-28 16:47:13 +0300

    Added lesson 1 filles

---
+++ lesson-1/foobar.rb
@@ -0,0 +1,14 @@
+def foobar(first, second)
+  if first == 20 or second == 20
+    puts "Результат: #{second}"
+  else
+    puts "Результат: #{first + second}"
+  end
+end
+
+puts "Введи первое число:"
+first_number = gets.to_i
+puts "Введи второе число:"
+second_number = gets.to_i
+
+foobar(first_number, second_number)
