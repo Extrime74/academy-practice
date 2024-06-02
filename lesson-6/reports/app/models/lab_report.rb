1a576edaaf9c38a3815c8181aa2cbbda6e710de0
Alexander Posukhovskiy <extrime74@gmail.com>
2024-06-02 15:09:53 +0300

    Minor changes

--- lesson-6/reports/app/models/lab_report.rb
+++ lesson-6/reports/app/models/lab_report.rb
@@ -2,5 +2,5 @@ class LabReport < ApplicationRecord
   belongs_to :user
   validates :title, length: { maximum: 250 }
   validates :description, length: { maximum: 500 }
-  validates :grade, inclusion: { in: %w(A B C D E FX F) }
+  validates :grade, inclusion: { in: %w[A B C D E FX F] }
 end
