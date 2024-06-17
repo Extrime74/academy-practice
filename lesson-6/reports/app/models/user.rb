c632071478d2bcff397f7a992f96727919cb35d5
Alexander Posukhovskiy <extrime74@gmail.com>
2024-06-17 22:58:55 +0300

    Install Devise

--- lesson-6/reports/app/models/user.rb
+++ lesson-6/reports/app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :lab_reports, dependent: :destroy
   validates :email, presence: true, length: { maximum: 150 }, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :first_name, length: { maximum: 100 }
